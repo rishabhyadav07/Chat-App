c568021792b9792a80ef1a87787944d1f567947f
Rishabh Kumar <rishabhkumar6904@gmail.com>
2024-03-09 19:11:40 +0530

    initial commit

--- front_end/src/components/sidebar/LogoutButton.jsx
+++ front_end/src/components/sidebar/LogoutButton.jsx
@@ -5,12 +5,13 @@ const LogoutButton = () => {
 	const { loading, logout } = useLogout();
 
 	return (
-		<div className='mt-auto'>
+		<div className='mt-2 inline-flex' >
 			{!loading ? (
 				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
 			) : (
 				<span className='loading loading-spinner'></span>
 			)}
+			<div className="text-blue-200 ml-1">Logout</div>
 		</div>
 	);
 };
