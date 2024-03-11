aeb7be2d00e50244fd3372a052a21a44ba12cf6e
Rishabh Kumar <rishabhkumar6904@gmail.com>
2024-03-11 19:38:35 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -21,12 +24,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/",(req,res) => {
-//     //root route http://localhost:5000/
-//     res.send("hello world,rishabh kumar yadav!!");
-// })
+app.use(express.static(path.join(__dirname,"/front_end/dist")));
 
-
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"front_end","dist","index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
